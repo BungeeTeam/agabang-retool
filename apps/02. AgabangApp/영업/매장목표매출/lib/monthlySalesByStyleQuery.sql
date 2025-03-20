0b8cf681475c1dd36bac512ada4990913ab349b9
Cleave User <project@cleave.work>
2025-03-20 11:06:04 +0000

    add query

--- apps/02. AgabangApp/영업/매장목표매출/lib/monthlySalesByStyleQuery.sql
+++ apps/02. AgabangApp/영업/매장목표매출/lib/monthlySalesByStyleQuery.sql
@@ -1,5 +1,6 @@
 SELECT 
   YEAR(sale_dt) as time_unit,
+  sales_type,
   br_cd,
   biz_cd,
   sub_br_cd,
@@ -15,11 +16,21 @@ SELECT
   item,
   item_nm,
   onoff_flag,
+  CASE
+      WHEN it = '8' THEN '용품'
+      WHEN it != '8' AND sale_dt <= season_end_dt THEN '정상'
+      ELSE '이월'
+    END AS sales_type,
+  first_lv_class,
+  second_lv_class,
   SUM(sales_price) / 1000000 as rev
 FROM agabang_dw.daily_shop_sales_by_dimension
 WHERE date_format(sale_dt, '%m-%d') <= date_format(DATE '{{ yearlyDate.value }}', '%m-%d') AND MONTH(sale_dt) = {{ monthSelect.value + 1 }}
 GROUP BY
   YEAR(sale_dt),
+  sales_type,
+  first_lv_class,
+  second_lv_class,
   br_cd,
   biz_cd,
   sub_br_cd,
