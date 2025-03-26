8ad126b0c3b2965052aa62af7cd14ef2ed10d718
클리브 <project@cleave.work>
2025-03-26 08:54:42 +0000

    Unprotecting revStatTblModule2

--- apps/99. Module/revStatTblModule2/lib/expectedTargetSalesQuery.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  br_cd,
-  date_format(year_month, '%y-%m') as ym,
-  SUM(target_sales) / 1000000 as target_sales
-FROM agabang_dw.yearly_target_sales_by_shop 
-WHERE shop_cd = ''
-GROUP BY br_cd, year_month
