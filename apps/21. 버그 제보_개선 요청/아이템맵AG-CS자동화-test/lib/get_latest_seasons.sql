505c9b8bb010096bcd0928d6dcaff659d402836d
chaewonlim9702 <chaewon@bungee.work>
2025-09-18 12:55:05 +0900

    주석 삭제

--- apps/21. 버그 제보_개선 요청/아이템맵AG-CS자동화-test/lib/get_latest_seasons.sql
+++ apps/21. 버그 제보_개선 요청/아이템맵AG-CS자동화-test/lib/get_latest_seasons.sql
@@ -42,18 +42,3 @@ FROM season_comparison
 WHERE previous_30days_qty > 0  -- 이전 30일 판매가 있었던 시즌만 비교
 ORDER BY growth_rate_percent DESC
 LIMIT {{ variable1 }};
-
-
--- SELECT
---     season_cd,
---     season_nm,
---     concat(year_cd,season_cd) as year_season_cd,
---     SUM(sales_qty) as total_sales_qty
--- FROM agabang_dw.daily_shop_sales_by_dimension
--- WHERE year_nm = toString(toYear(today()))
---   AND sale_dt >= today() - INTERVAL 1 MONTH
---   AND season_cd != '0'
---   AND br_cd = '{{ variable0 }}'
--- GROUP BY year_cd, season_cd, season_nm
--- ORDER BY 4 desc
--- LIMIT {{ variable1 }};
