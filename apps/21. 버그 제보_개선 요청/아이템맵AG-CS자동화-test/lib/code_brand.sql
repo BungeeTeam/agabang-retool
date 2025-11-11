d9c0bc78fd9698e6d1c16eff41dd197725b1c44b
클리브 <project@cleave.work>
2025-11-11 04:43:07 +0000

    Unprotecting 아이템맵AG-CS자동화-test

--- apps/21. 버그 제보_개선 요청/아이템맵AG-CS자동화-test/lib/code_brand.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-br_cd,
-CASE 
-  WHEN br_cd = '75' THEN '해외브랜드'
-  WHEN br_cd = '76' THEN '더블하트/스와들업'
-  ELSE br_nm END as br_nm
-FROM agabang.plbrcd
-WHERE br_cd in ('01', '02', '58', '71', '72', '73', '74', '75', '76', '77', '79')
--- apps/21. 버그 제보_개선 요청/아이템맵AG-CS자동화-test/lib/get_category_list.sql
+++
@@ -1,5 +0,0 @@
-select * 
-  from item_md_category 
- -- where cast(br_cd as int) = {{ var_brand_code.value }}
-where br_cd = '01'
-  order by id asc
