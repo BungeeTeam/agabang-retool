d1ce0b6d1fd8ac510688959688c93f3b3329d717
클리브 User <project@cleave.work>
2025-12-16 03:01:38 +0000

    chanfe biz code

--- apps/04. 발주최적화/매장초도분배/lib/getSelectedGroupShop2.sql
+++ apps/04. 발주최적화/매장초도분배/lib/getSelectedGroupShop2.sql
@@ -6,7 +6,7 @@ select
   t2.user_nm
 from (
   select distinct shop_cd from agabang.cfshgrpd
-  where grp_id = '{{ groupSelect2.value }}' and use_yn = 'Y' and biz_div = '{{ bizcd2.value }}'
+  where grp_id = '{{ groupSelect2.value }}' and use_yn = 'Y' and biz_div = 'A4' -- {{ bizcd2.value }}
 ) as t1
 left join (
   select distinct shop_cd, shop_nm, tp_nm, team_nm, user_nm from agabang_dw.dim_shop
