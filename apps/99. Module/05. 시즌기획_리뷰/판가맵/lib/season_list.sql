15842821e6ced69fd5d99b8b998aebd489d6bb05
클리브 User <project@cleave.work>
2026-01-06 06:03:43 +0000

    season_list order 수정

--- apps/99. Module/05. 시즌기획_리뷰/판가맵/lib/season_list.sql
+++ apps/99. Module/05. 시즌기획_리뷰/판가맵/lib/season_list.sql
@@ -1,9 +1,10 @@
 select
     year_sesn_nm_eng,
     year_sesn_nm_kor,
-    year_cd, sesn_cd
+    year_cd, sesn_cd,
+    CAST(year_nm as int)*10 + CAST(sesn_cd as int) as orderidx
 from agabang_dw.retooldb_code_season
 where sesn_cd in ('0', '1', '3', '5', '7')
 and toInt8(substring(year_sesn_nm_eng,1,2)) >= 20
 and toInt8(substring(year_sesn_nm_eng,1,2)) < 40
-order by 1 desc
+order by orderidx desc;
