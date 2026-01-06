df242576b8e61a68c791cb0fb34710336139ae2b
클리브 User <project@cleave.work>
2026-01-06 05:43:01 +0000

    원가 컬럼 추가

--- apps/02. AgabangApp/70. 설정_관리/01. 제품 정보 관리/lib/update_production.js
+++ apps/02. AgabangApp/70. 설정_관리/01. 제품 정보 관리/lib/update_production.js
@@ -30,12 +30,16 @@ const mappings = {
   '기획량4': 'color_4_qty',
   '색상5': 'color_5_nm',
   '기획량5': 'color_5_qty',
+  '확정원가(v-,원)': 'cost_price_final',
+  '사전원가(v-,원)': 'cost_price'
 };
 
 // 필드별 데이터 타입 정의
 const fieldTypes = {
   'sale_price': 'Int32',
   'sale_price_final': 'Int32',
+  'cost_price': 'Int32',
+  'cost_price_final': 'Int32',
   'item_md_category_id': 'Int32',
   'color_1_qty': 'Int32',
   'color_2_qty': 'Int32',
@@ -350,166 +354,3 @@ ${updateClause};
     notificationType: 'error',
   });
 }
-//  // production_temp_data에서 데이터를 가져옵니다.
-//  const data = production_temp_data.value;
-
-//  // 엑셀 필드와 데이터베이스 필드의 매핑을 정의합니다.
-//  const mappings = {
-//    '브랜드코드': 'br_cd',
-//    '품번': 'sty_cd',
-//    '소재세부': 'fabric',
-//    'MD제안납기': 'deli_dt',
-//    '확정납기': 'adj_deli_dt',
-//    '디자이너': 'designer_nm',
-//    //  '샘플넘버': 'preview_cd',
-//    '품평번호': 'preview_cd',
-//    '품평점수': 'preview_score',    
-//    '품평등급': 'preview_grade',    
-//    '품평의견': 'opinion',   
-//    '성별': 'sex_nm',
-//    '대분류': 'cat_raw_nm',
-//    '중분류': 'cat_nm',
-//    '소분류': 'item_raw_nm',
-//    '예상TAG가(원)': 'sale_price',
-//    '확정TAG가(원)': 'sale_price_final',
-//    '매핑카테고리ID': 'item_md_category_id',
-    
-//    '색상': 'color_1_nm',
-//    '기획량': 'color_1_qty',
-//    '색상2': 'color_2_nm',
-//    '기획량2': 'color_2_qty',
-//    '색상3': 'color_3_nm',
-//    '기획량3': 'color_3_qty',
-//    '색상4': 'color_4_nm',
-//    '기획량4': 'color_4_qty',
-//    '색상5': 'color_5_nm',
-//    '기획량5': 'color_5_qty',
-//    };
-
-//  // INSERT 문에 사용할 필드 목록을 생성합니다.
-//  const dbFields = Object.values(mappings);
-
-//  // 각 행의 값을 저장할 배열을 초기화합니다.
-//  const valuesList = [];
-
-//  // 0.5초씩 쉬는 함수
-//  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-//  // 데이터 처리를 시작합니다.
-//  data.forEach(row => {
-//    // 각 필드의 값을 저장할 배열을 초기화합니다.
-//    const valueRow = [];
-
-//    if(row){
-//      // 각 매핑에 대해 값을 처리합니다.
-//      for (const [excelField, dbField] of Object.entries(mappings)) {
-//        //  console.log(`excelField: ${excelField}, row: ${JSON.stringify(row)}`)
-//        let value = row[excelField] ? String(row[excelField])?.trim() : null;
-  
-//        // '납기' 필드의 경우 날짜 형식을 변환합니다.
-//        if ( ( excelField === 'MD제안납기' || excelField === '확정납기' ) && value) {
-//          let convertedDate = null;
-  
-//          // 'MM/DD' 형식인지 확인합니다.
-//          const dateParts = value.split('/');
-//          if (dateParts.length === 2) {
-//            let month = dateParts[0].padStart(2, '0');
-//            let day = dateParts[1].padStart(2, '0');
-  
-//            convertedDate = `2024-${month}-${day}`;
-//            // 날짜 유효성을 확인합니다.
-//            const dateObj = new Date(convertedDate);
-//            if (isNaN(dateObj.getTime())) {
-//              convertedDate = null;
-//            }
-//          } else {
-//            // 다른 형식의 날짜 처리
-//            const dateObj = new Date(value);
-//            if (!isNaN(dateObj.getTime())) {
-//              const year = dateObj.getFullYear();
-//              const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
-//              const day = dateObj.getDate().toString().padStart(2, '0');
-//              convertedDate = `${year}-${month}-${day}`;
-//            } else {
-//              convertedDate = null;
-//            }
-//          }
-  
-//          value = convertedDate;
-//        }
-  
-//        // '수량' 의 경우 콤마를 제거합니다.
-//        if((excelField=='기획량'||excelField=='기획량2'||excelField=='기획량3'||excelField=='기획량4'|| excelField=='기획량5'||excelField=='예상TAG가(원)'||excelField=='확정TAG가(원)'||excelField=='확정TAG가(원)')
-//            && value ) {
-//          if(value === '-'){
-//            value = null;
-//          }
-//          else if(value.indexOf(',')>=0){
-//            value = value.replace(",", "")
-//          }
-//        }
-  
-//        // 값이 있는 경우 작은따옴표를 이스케이프합니다. + 하이픈 제거 추가
-//        if (value !== null && value !== undefined) {
-//          //  value = value.replace(/-/g, ""); //하이픈 제거 추가
-//          value = value.replace(/'/g, "''");
-//          value = `'${value}'`;
-//        } else {
-//          value = 'NULL';
-//        }
-  
-//        valueRow.push(value);
-//      }
-  
-//      // 'STY_CD'가 NULL인 경우 해당 행을 건너뜁니다.
-//      if (valueRow[dbFields.indexOf('sty_cd')] === 'NULL' || !valueRow[dbFields.indexOf('sty_cd')]) {
-//        return;
-//      }
-  
-//      valuesList.push(`(${valueRow.join(', ')})`);
-//    }
-  
-//  });
-
-//  // 값이 없는 경우 쿼리를 실행하지 않습니다.
-//  if (valuesList.length === 0) {
-//    console.error('유효한 데이터가 없습니다.');
-//    throw new Error('유효한 데이터가 없습니다.');
-//  }
-
-//  // 업데이트할 필드 목록 생성 (STY_CD 제외)
-//  const updateFields = dbFields.filter(field => field !== 'sty_cd');
-
-//  // UPDATE 절 생성
-//  const updateClause = updateFields.map(field => {
-//    return `${field} = EXCLUDED.${field}`;
-//  }).join(',\n');
-
-//  // 최종 INSERT 쿼리 생성
-//  const insertQuery = `
-//  INSERT INTO item_md_info (${dbFields.join(', ')})
-//  VALUES
-//  ${valuesList.join(',\n')}
-//  ON CONFLICT (sty_cd) DO UPDATE SET
-//  ${updateClause};
-//  `;
-
-//  console.log(insertQuery)
-//  var_update_production_query.setValue(insertQuery)
-
-//  // 0.5초 대기
-//  await sleep(500);
-
-//  // SQL 트리거
-//  update_production_query.trigger();
-
-//  // 알림
-//  fileDropzone3.clearValue();
-//  production_temp_data.setValue([]);
-//  query_get_info.trigger();
-//  modalFrame_production_upload.setHidden(true);
-//  utils.showNotification({
-//    title: '업데이트 성공',
-//    description: '업데이트 내용이 반영되었습니다',
-//    notificationType: 'success',
-//  });
